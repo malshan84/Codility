75c11be6b44e733482647e6a5a45f1459da26b10
malshan <malshan84@gmail.com>
2021-03-07 19:30:07 +0900

    메서드 이름 수정

--- src/main/java/Lesson3/FrogJmp.java
+++ src/main/java/Lesson3/FrogJmp.java
@@ -2,14 +2,7 @@ package Lesson3;
 
 // https://app.codility.com/demo/results/trainingYUP4RD-D66/
 public class FrogJmp {
-	public int solution(int X, int Y, int D) {
-		// write your code in Java SE 8
-		int k = (Y-X)%D == 0 ? (Y-X)/D : ((Y-X)/D)+1;
-		return k;
-	}
-
-	public static void main(String[] args) {
-		int result = new FrogJmp().solution(10,20,30);
-		System.out.println(result);
+	public int getMinimalJumpCount(int X, int Y, int D) {
+		return (Y-X)%D == 0 ? (Y-X)/D : ((Y-X)/D)+1;
 	}
 }
