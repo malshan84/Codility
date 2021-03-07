cbbef6c8a3d7081ff63787603201ddff69d363d5
malshan <malshan84@gmail.com>
2021-03-07 19:45:08 +0900

    변수 이름 리펙토링

--- src/main/java/Lesson3/PermMissingElem.java
+++ src/main/java/Lesson3/PermMissingElem.java
@@ -2,13 +2,13 @@ package Lesson3;
 // https://app.codility.com/demo/results/trainingK53ZGV-Z45/
 public class PermMissingElem {
 	public int getMissingElement(int[] A) {
-		int sumA = 0;
-		int total = A.length+1;
+		int sumOfElements = 0;
+		int sumOfIntegersToRange = A.length+1;
 
 		for(int i=0; i<A.length; i++) {
-			sumA+=A[i];
-			total+=i+1;
+			sumOfElements+=A[i];
+			sumOfIntegersToRange+=i+1;
 		}
-		return total-sumA;
+		return sumOfIntegersToRange-sumOfElements;
 	}
 }
