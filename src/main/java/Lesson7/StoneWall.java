4f445fd997deb0fc5f11e414c5a32d36de20c346
malshan <malshan84@gmail.com>
2021-03-18 20:39:38 +0900

    StoneWall 100%

--- src/main/java/Lesson7/StoneWall.java
+++ src/main/java/Lesson7/StoneWall.java
@@ -1,7 +1,24 @@
 package Lesson7;
 
+import java.util.Stack;
+
 public class StoneWall {
-	public int solution(int[] H){
-		return H.length;
+	public int getMinNumOfBlocks(int[] H){
+		Stack<Integer> stack = new Stack<>();
+		int numOfBlocks = 0;
+		for(int h : H) {
+			if(!stack.isEmpty() && stack.peek() >= h) {
+				while (!stack.isEmpty() && stack.peek() > h) {
+					stack.pop();
+					numOfBlocks++;
+				}
+			}
+
+			if(stack.isEmpty() || stack.peek() < h) {
+				stack.push(h);
+			}
+		}
+		return numOfBlocks+stack.size();
+
 	}
 }
--- src/test/java/Lesson7/StoneWallTest.java
+++ src/test/java/Lesson7/StoneWallTest.java
@@ -3,7 +3,6 @@ package Lesson7;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class StoneWallTest {
 
@@ -13,14 +12,13 @@ class StoneWallTest {
 //	H[0] = 8    H[1] = 8    H[2] = 5
 //	H[3] = 7    H[4] = 9    H[5] = 8
 //	H[6] = 7    H[7] = 4    H[8] = 8
-// 8 8 5 7 9 8 7 4 8
-// - - 5 7 8
-//	   1 2 3 4 5 6 7
-
+//
+//4 8
+//5
 	@Test
 	void test1() {
 		assertThat(
-				stoneWall.solution(new int[]{8,8,5,7,9,8,7,4,8})
+				stoneWall.getMinNumOfBlocks(new int[]{8,8,5,7,9,8,7,4,8})
 		).isEqualTo(7);
 	}
 }
